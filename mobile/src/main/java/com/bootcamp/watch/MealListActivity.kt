8201fec29b8f133dadb7d65d5b39f41630d74abc
cielio <cielion@hotmail.com>
2021-03-31 19:07:20 -0300

    Initial commit

---
+++ mobile/src/main/java/com/bootcamp/watch/MealListActivity.kt
@@ -0,0 +1,24 @@
+package com.bootcamp.watch
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MealListActivity : AppCompatActivity(), MealListAdapter.Callback {
+  private var adapter: MealListAdapter? = null
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    val meals = MealStore.fetchMeals(this)
+    adapter = MealListAdapter(meals, this)
+    list.adapter = adapter
+    list.layoutManager = LinearLayoutManager(this)
+  }
+
+  override fun mealClicked(meal: Meal) {
+    // TODO: Handle this meal click!
+  }
+}
